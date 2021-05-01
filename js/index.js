1559055f6cff8ffe08c0858da8cd4fd80a157606
ValentinVaninetti <valenga20@gmail.com>
2021-05-01 01:19:20 -0300

    adas

--- js/index.js
+++ js/index.js
@@ -1,25 +1,42 @@
 "use strict";
 
-let btn = document.querySelector("#botoncaptcha");
+    let input = document.querySelector("#captchaingresado");
+    let btn = document.querySelector("#botoncaptcha");
+    let check = document.querySelector("#Check");
+    check.addEventListener("click", CrearCaptcha);
+    btn.addEventListener("click", verificarCaptcha);
 
-btn.addEventListener("click", CrearCaptcha());
 
-
-function CrearCaptcha(){
-  console.log ('entre');
-
-  let text = "";
-  let possible = "ABCDEFGHIJKLMNOPQRSTUVWXYZabcdefghijklmnopqrstuvwxyz0123456789";
-
-  for (let i = 0; i < 5; i++){
-    text += possible.charAt(Math.floor(Math.random() * possible.length));
-  }
-    console.log (text);
-    document.querySelector("#captcha").innerHTML=text;
+    
+    function CrearCaptcha(){
+      console.log ('entre');
       
+      let text = "";
+      let possible = "ABCDEFGHIJKLMNOPQRSTUVWXYZabcdefghijklmnopqrstuvwxyz0123456789";
+      
+      for (let i = 0; i < 5; i++){
+        text += possible.charAt(Math.floor(Math.random() * possible.length));
+      }
+      console.log (text);
+      document.querySelector("#captcha").innerHTML=text;
+      return text;   
+    }
+    
 
-  return text; 
-}
+        function verificarCaptcha(){
+          event.preventDefault();
+          if (input.value == text){
+          alert("Bien")
+
+              } else {
+                alert("morite")
+              }
+                
+
+        }
+    
+  
+
      
      
      //Y LA PERRA SEGUIA Y SEGUIA COMO VAS A USAR ONCLICK PAPI SI NO ESTA EN EL HTML
@@ -53,3 +70,40 @@ function CrearCaptcha(){
 
 //let btnenviar = document.querySelector ("btn-enviar");
 //btnenviar.addEventListener ("click", validarcaptcha)
+
+
+
+
+
+
+
+
+
+
+//let btnEnviar = document.getElementById("send");
+
+//btnEnviar.addEventListener("click", function(){
+                              // Verificar que lo que escribió el usuario sea igual a 8
+                              //Primero busco el input
+  //let inputCaptcha = document.getElementById("captcha");
+                               //Verifico el dato
+  //let divResultado = document.getElementById("resultado");
+  //if (inputCaptcha.value == 8) {
+                               //El dato es igual a 8
+                                //alert("esta bien");
+                                //divResultado.innerText = "Mensaje enviado";
+
+    //let encabezado = document.createElement("h1");
+    //encabezado.innerText = "Mensaje Enviado";
+    //divResultado.appendChild(encabezado)
+  //} else {
+                                // El dato es distinto a 8
+                                //alert("esta mal");
+                                //divResultado.innerHTML = "Captcha incorrecto"
+    //let link = document.createElement("a");
+    //link.innerText = "Ir a google";
+    //link.href = "www.google.com";
+    //divResultado.appendChild(link);
+    
+  //}
+//});
